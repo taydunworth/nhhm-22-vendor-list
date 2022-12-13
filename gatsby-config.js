f7c180137519838070862b2105762f17058c1e6b
Taylor Cox <taylorashtoncox@gmail.com>
2022-12-13 09:47:34 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Nashville Handmade Holiday Market 22 Vendor List`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sass"]
+};
